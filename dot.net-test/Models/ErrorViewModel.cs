29738328121c48864a037b31fa736b4d80a16d0a
Mohammad Asheef <mohammad.asheef@caypro.io>
2024-04-05 14:26:38 +0530

    Add project files.

---
+++ dot.net-test/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace dot.net_test.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
